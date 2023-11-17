a869cf75e50233a83282a8350c81390432bd4e07
Perishte <javascript_9595@bk.ru>
2023-11-17 16:14:26 +0500

    added comments into js

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 const API_KEY = '236c4b88131b4888ae662754231711';
 const API_URL = 'https://api.weatherapi.com/v1/current.json?';
 const searchBox = document.querySelector('.searchBox');
+const loader = document.querySelector('.dot-pulse ');
 
+//Event with callback function
 searchBox.addEventListener("keypress", async function searchCity(e, city) {
   const card = document.querySelector('.card');
   const errorWrap = document.querySelector('.errorWrap');
   if(e.key === 'Enter' && e.target.value) {
     city = searchBox.value;
+    //catch response from server
     const response = await fetch(API_URL + 'key=' + API_KEY + '&q=' + city);
+    //The end of catch response from server
+    //Checking for errors with status code
+    //Page was not found Block
     if(response.status === 404) {
        errorWrap.style.display = 'block';
        errorWrap.textContent = 'Server can\'t find requested resource';
        card.style.display = 'none';
+      loader.style.display = 'block';
        return;
+      //The end of Page was not found Block
+      //Bad request from server checking
     } else if (response.status === 400) {
       errorWrap.style.display = 'block';
       errorWrap.textContent = 'Please enter correct name of city: ';
       card.style.display = 'none';
+      loader.style.display = 'block';
       return;
     }
+    //The end of Bad request from server checking
+    //The end of Checking for errors with status code
+    //formatting from json into object
     const data = await response.json();
     searchBox.disabled=true;
     console.log(data);
@@ -26,6 +39,7 @@ searchBox.addEventListener("keypress", async function searchCity(e, city) {
     card.style.display = 'flex';
     searchBox.disabled=false;
     searchBox.value = '';
+    //get data from server and show on the web-page
     let location = document.querySelector('.location');
     location.textContent = data.location.name+', '+data.location.country;
     const date1 = document.querySelector('.date');
@@ -49,12 +63,21 @@ searchBox.addEventListener("keypress", async function searchCity(e, city) {
     const wind = document.querySelector('.wind-value');
     wind.textContent = data.current.wind_kph + 'kph';
     const country = document.querySelector('#country');
+    //get data from server and show on the web-page
+    //hide data
     country.style.display = 'none';
+    loader.style.display = 'none';
+    //The end of hide data
   } else {
+    //if no data show error
     errorWrap.style.display = 'block';
     errorWrap.textContent = 'Please enter the name of the city and press enter to see the results';
     card.style.display = 'none';
+    loader.style.display = 'block';
     return;
+    //if no data show error the end
   }
 });
+
+//The end of Event with callback function
 
