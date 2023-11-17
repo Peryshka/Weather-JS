4f90b1fc1c3f39407ad038aa40d864a437419f6f
Perishte <javascript_9595@bk.ru>
2023-11-17 17:35:27 +0500

    formatting

--- main.js
+++ main.js
@@ -7,20 +7,20 @@ const loader = document.querySelector('.dot-pulse ');
 searchBox.addEventListener("keypress", async function searchCity(e, city) {
   const card = document.querySelector('.card');
   const errorWrap = document.querySelector('.errorWrap');
-  if(e.key === 'Enter' && e.target.value) {
+  if (e.key === 'Enter' && e.target.value) {
     city = searchBox.value;
     //catch response from server
     const response = await fetch(API_URL + 'key=' + API_KEY + '&q=' + city);
     //The end of catch response from server
     //Checking for errors with status code
     //Page was not found Block
-    if(response.status === 404) {
-       errorWrap.style.display = 'block';
-       errorWrap.textContent = 'Server can\'t find requested resource';
-       card.style.display = 'none';
+    if (response.status === 404) {
+      errorWrap.style.display = 'block';
+      errorWrap.textContent = 'Server can\'t find requested resource';
+      card.style.display = 'none';
       loader.style.display = 'block';
       searchBox.value = '';
-       return;
+      return;
       //The end of Page was not found Block
       //Bad request from server checking
     } else if (response.status === 400) {
@@ -35,31 +35,35 @@ searchBox.addEventListener("keypress", async function searchCity(e, city) {
     //The end of Checking for errors with status code
     //formatting from json into object
     const data = await response.json();
-    searchBox.disabled=true;
-    console.log(data);
+    searchBox.disabled = true;
     errorWrap.style.display = 'none';
     card.style.display = 'flex';
-    searchBox.disabled=false;
+    searchBox.disabled = false;
     searchBox.value = '';
     //get data from server and show on the web-page
     let location = document.querySelector('.location');
-    location.textContent = data.location.name+', '+data.location.country;
+    location.textContent = data.location.name + ', ' + data.location.country;
     const date1 = document.querySelector('.date');
     const localtime = data.location.localtime.split(' ')
     const [date, time] = localtime;
     const now = new Date(date)
-    const formattedDate = now.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+    const formattedDate = now.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })
     date1.textContent = formattedDate;
     const time2 = document.querySelector('.time');
     time2.textContent = time;
     const mainCondImg = document.querySelector('.main-cond');
-    mainCondImg.src =`https:${data.current.condition.icon}`;
+    mainCondImg.src = `https:${data.current.condition.icon}`;
     const temp = document.querySelector('.temp');
     temp.textContent = Math.round(data.current.temp_c) + '°c';
     const condition = document.querySelector('.cond');
     condition.textContent = data.current.condition.text;
     const temp_feels = document.querySelector('.temp-feels');
-    temp_feels.textContent = data.current.feelslike_c + '°c' ;
+    temp_feels.textContent = data.current.feelslike_c + '°c';
     const humidity = document.querySelector('.humid-value');
     humidity.textContent = data.current.humidity + '%';
     const wind = document.querySelector('.wind-value');
@@ -82,4 +86,5 @@ searchBox.addEventListener("keypress", async function searchCity(e, city) {
 });
 
 //The end of Event with callback function
+
 
